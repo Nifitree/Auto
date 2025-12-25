a73022417bf69dd8a56569019eaa30d0fa46d0eb
Nifitree <nifitree28@gmail.com>
2025-12-25 13:18:30 +0700

    mutual

--- mutual_1.py
+++ mutual_1.py
@@ -3,11 +3,9 @@ from mutual_core import mutual_main, run_mutual_transaction, finish_transaction,
 if __name__ == "__main__":
     print(f"\n{'='*50}\n[*] Running Mutual Service 1...")
     try:
+        if not mutual_main(): exit()
         app, main_window = connect_main_window()
-        # Service 1 ไม่ต้องเข้า Main Menu (เพราะอยู่หน้าแรก หรือ Logic เดิมไม่ได้เรียก)
-        # แต่ถ้าต้องเข้า ให้ uncomment บรรทัดล่าง
-        # if not mutual_main(): exit() 
-        
+
         run_mutual_transaction(main_window, S_CFG['MUTUAL_1_TITLE'], barcode_id=S_CFG['BARCODE_EDIT_AUTO_ID'])
         finish_transaction(main_window)
         print("[V] Service 1 Success!")
