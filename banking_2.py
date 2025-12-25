6291e98e8371662d175f43cb2487bfb55bfbe46b
Nifitree <nifitree28@gmail.com>
2025-12-25 10:30:21 +0700

    banking

--- banking_2.py
+++ banking_2.py
@@ -1,4 +1,4 @@
 from banking_core import run_service, S_CFG
 
 if __name__ == "__main__":
-    run_service("Banking_2", S_CFG["BANKING_2_TITLE"], use_search=True)
+    run_service("Banking_2", S_CFG["BANKING_2_TITLE"], use_main=True, double_next=True)
--- banking_4.py
+++ banking_4.py
@@ -1,4 +1,4 @@
 from banking_core import run_service, S_CFG
 
 if __name__ == "__main__":
-    run_service("Banking_4", S_CFG["BANKING_4_TITLE"], use_search=True)
+    run_service("Banking_4", S_CFG["BANKING_4_TITLE"], use_main=True, double_next=True)
