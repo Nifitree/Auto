7e51203360ca8179d1181d0fe19e636435c7aa65
Nifitree <nifitree28@gmail.com>
2026-01-08 13:54:32 +0700

    เครื่องใช้ไฟฟ้า

--- emsjumbo_com2.py
+++ emsjumbo_com2.py
@@ -40,9 +40,9 @@ NEXT_TITLE = "ถัดไป"
 
 # Specific Config
 try:
-    CFG = CONFIG["EMS_JUMBO_COM"]
+    CFG = CONFIG["EMS_JUMBO"]
 except KeyError:
-    print("[X] ไม่พบ Section [EMS_JUMBO_COM] ใน config.ini")
+    print("[X] ไม่พบ Section [EMS_JUMBO] ใน config.ini")
     sys.exit(1)
 
 ctx = AppContext(window_title_regex=WINDOW_TITLE)
